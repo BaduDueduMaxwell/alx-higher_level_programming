171e094a1933bebd5daaf7f5cb2ea31eaa47fdb4
Maxwell <duedumaxwell43@gmail.com>
2022-06-28 10:47:56 -0700

    task-1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 5:
+    print("greater than 5")
+elif number == 0:
+    print("0")
+elif number < 6:
+    print("less than 6 and not 0")
