ebdb4c998262da82f85046ae975a6ee9bfda7c75
Maxwell <duedumaxwell43@gmail.com>
2022-07-02 16:51:37 +0000

    task-1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/bin/bash/python3
 if __name__ = "__main__":
-    import calculator_1
+    import calculator_1 as add, sub, mul, div
     a = 10
     b = 5
-    print(f"{a + b}")
-    print(f"{a - b}")
-    print(f"{a * b}")
-    print(f"{a / b}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
