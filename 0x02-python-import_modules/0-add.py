a5de75852bb920a694a4be31153b8f3159b989db
Maxwell <duedumaxwell43@gmail.com>
2022-07-02 14:40:46 +0000

    task-0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
-#!/usr/bin/pythom3
-import add_0
-if __name__ = "__main__":
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import add_0 as add
     a = 1
     b = 2
-    print("{} + {} + {}".format(a,b, add.add(a, b)))
+    print("{} + {} = {}".format(a, b, add.add(a, b)))
