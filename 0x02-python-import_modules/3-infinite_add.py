c53f0e819ca0ab9dc69e1464c06fd6c9e762e28c
Maxwell <duedumaxwell43@gmail.com>
2022-07-04 17:19:44 +0000

    task-3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+#total arguments
+total = len(sys.argv)
+
+#argument passed
+for i in range(1, total):
+    print(sys.argv[i], end= " ")
+#addition of numbers
+
+sum = 0
+
+#using argparse module
+for i in range(i, n):
+    sum += int(sys.argv[i])
+print(f"{sum}")
+
