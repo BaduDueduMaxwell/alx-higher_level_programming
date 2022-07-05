aa68405633fdf8d321e32031abe0ac04abcedbd0
Maxwell <duedumaxwell43@gmail.com>
2022-07-05 14:09:46 +0000

    task-3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,17 +2,9 @@
 if __name__ "__main__":
     import sys
 #total arguments
-    total = len(sys.argv)
-
-#argument passed
-    for i in range(1, total):
-        print(sys.argv[i], end= " ")
-#addition of numbers
-
-    sum = 0
-
-#using argparse module
-    for i in range(i, n):
-        sum += int(sys.argv[i])
-    print(f"{sum}")
+    total = 0
+    
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i]
+    print(f"{total}")
 
