47cd2e465713cae3a3a01ed753c5aacf42698212
Badu Duedu Maxwell <102438296+BaduDueduMaxwell@users.noreply.github.com>
2022-07-05 18:35:07 +0000

    task-6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    import sys
-    import calculator_1 as calc
-    no = len(sys.argv)
-    op = "+-*/"
-    if no != 4:
+    # check usage
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    if sys.argv[2] not in op:
+        sys.exit(1)
+
+    if sys.argv[2] not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    c = sys.argv[2]
-    if c == "+":
-        print("{} {} {} = {}".format(a, c, b, calc.add(a, b)))
-    elif c == "-":
-        print("{} {} {} = {}".format(a, c, b, calc.sub(a, b)))
-    elif c == "*":
-        print("{} {} {} = {}".format(a, c, b, calc.mul(a, b)))
-    elif c == "/":
-        print("{} {} {} = {}".format(a, c, b, calc.div(a, b)))
+        sys.exit(1)
+
+    a, op, b = sys.argv[1:]
+
+    # choose operation
+    if op == '+':
+        func = add
+    elif op == '-':
+        func = sub
+    elif op == '*':
+        func = mul
+    else:
+        func = div
+
+    # perfom calculation and print result
+    print("{} {} {} = {:d}".format(a, op, b, func(int(a), int(b))))
