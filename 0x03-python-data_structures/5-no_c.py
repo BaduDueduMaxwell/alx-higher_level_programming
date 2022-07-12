709dfb142be896a5b883fb0aa4331aa97e613fb1
Maxwell <duedumaxwell43@gmail.com>
2022-07-12 21:40:24 +0000

    task-5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@ def no_c(my_string):
     a = ""
     for i in range(0, length):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            pass
+            continue
         a = a + my_string[i]
     return("".join(copy))
