537303ab9833f5a9f54aa88a4136035628b56d30
Maxwell <duedumaxwell43@gmail.com>
2022-07-13 21:53:44 +0000

    task-8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    for char in multiple_returns:
+        if sentence == "":
+            return None
+        else:
+            return sentence
