492f65eced106496b0d32caf08d38d560f0ca670
Maxwell <duedumaxwell43@gmail.com>
2022-07-13 22:12:54 +0000

    task-9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[0]):
-    highest_num = 0
     for num in range(my_list[0]):
+        highest_num = my_list[0]
         if num > highest_num:
             highest_num = num
             return highest_num
