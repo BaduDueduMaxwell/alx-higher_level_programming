e9fc153dada1cf68e7bf347d12685c6f1a853ad9
Maxwell <duedumaxwell43@gmail.com>
2022-07-14 22:19:42 +0000

    task-10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    multiples = []
     for multiple in range(len(my_list)):
         if multiple % 2 == 0:
             return True
         else:
             return False
+    return multiples
