a3ce5cccbe9d76a2cdf9e2b2797afe599319a0c3
kode <duedumaxwell43@gmail.com>
2024-03-22 11:46:00 -0700

    fixed positive condition

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+
+1;95;0c#!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number >= 0:
+if number > 0:
     print(f"{number} is positive")
 elif number < 0:
     print(f'{number} is negative')
