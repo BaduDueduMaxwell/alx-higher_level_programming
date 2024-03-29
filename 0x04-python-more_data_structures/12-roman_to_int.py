774c24aebedd37a5d4c5abff91ee786dc822fe6b
kode <duedumaxwell43@gmail.com>
2024-03-29 06:08:55 -0700

    task 12:pycodestyle fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,11 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-
-    roman_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    roman_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5,
+                 'I': 1}
     integer_value = 0
     for roman in reversed(roman_string):
         roman_key = roman_map[roman]
-        integer_value += roman_key if integer_value < roman_key * 5 else -roman_key
+        integer_value += roman_key if integer_value < roman_key * 5 \
+            else -roman_key
     return integer_value
