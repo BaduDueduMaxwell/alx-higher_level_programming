f5810fa85f2c050af461ecf41ec6ad7dee1f5c2a
kode <duedumaxwell43@gmail.com>
2024-03-29 06:03:59 -0700

    task 12: fixed not string error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
+    if not roman_string or type(roman_string) != str:
+        return 0
 
     roman_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     integer_value = 0
