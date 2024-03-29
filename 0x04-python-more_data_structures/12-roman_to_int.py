96e88a8eb4b17b07756cef2b64cf958f290168ca
kode <duedumaxwell43@gmail.com>
2024-03-29 06:00:54 -0700

    task 12:roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,23 +4,9 @@ def roman_to_int(roman_string):
     if not roman_string:
         return None
 
+    roman_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     integer_value = 0
-    for char in roman_string:
-        if char == 'I':
-            integer_value += 1
-        elif char == 'V':
-            integer_value += 5
-        elif char == 'X':
-            integer_value += 10
-        elif char == 'L':
-            integer_value += 50
-        elif char == 'C':
-            integer_value += 100
-        elif char == 'D':
-            integer_value += 500
-        elif char == 'M':
-            integer_value += 1000
-        elif char == 'I' and roman_string[roman_string.index(char) + 1] in 'VX':
-            integer_value += (roman_string[roman_string.index(char) + 1] in 'V' and 5 or 10) - 1
-            continue
+    for roman in reversed(roman_string):
+        roman_key = roman_map[roman]
+        integer_value += roman_key if integer_value < roman_key * 5 else -roman_key
     return integer_value
