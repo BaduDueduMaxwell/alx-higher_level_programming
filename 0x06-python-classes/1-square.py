3cc30f45e1bb7420861c458856669c1fd9557e85
kode <duedumaxwell43@gmail.com>
2024-04-04 17:25:15 -0700

    task 1:private instance attribute:"size"

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square.py by kode
+"""Defines a square"""
+
+
+class Square:
+    """Private instance attribute: size"""
+    def __init__(self, size):
+        self.__size = size
