dd2d5b8e7f0963c163fe939a49a83a2b8db45d20
kode <duedumaxwell43@gmail.com>
2024-04-06 16:35:31 -0700

    task 10: initializing objects of class correction

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 class MagicClass:
-    def not isinstance(radius, (int, float)):
-        raise TypeError("radius must be a number")
-    self.__radius = radius
+    """magic class setup"""
+    def __init__(self, radius):
+        if not isinstance(radius, (int, float)):
+            raise TypeError("radius must be a number")
+        self.__radius = radius
 
     def area(self):
+        """docstring for area"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """docstring for circumference"""
         return 2 * math.pi * self.__radius
