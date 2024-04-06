3b47adccc36c47928dfbfedb70baa956e6be503d
kode <duedumaxwell43@gmail.com>
2024-04-06 16:44:44 -0700

    task 10

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,15 +5,15 @@ import math
 
 class MagicClass:
     """magic class setup"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         if not isinstance(radius, (int, float)):
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """docstring for area"""
+        """Calculate the area of the circle."""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """docstring for circumference"""
+        """Calculate the circumference of the circle."""
         return 2 * math.pi * self.__radius
