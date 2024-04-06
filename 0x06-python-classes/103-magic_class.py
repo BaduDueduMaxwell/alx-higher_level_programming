ef4bf1faadd9445cd0849392deb757dbdbd84aae
kode <duedumaxwell43@gmail.com>
2024-04-06 16:30:33 -0700

    task 10

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class MagicClass:
+    def not isinstance(radius, (int, float)):
+        raise TypeError("radius must be a number")
+    self.__radius = radius
+
+    def area(self):
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
