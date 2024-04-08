5090fe5a1afe23f9a5571876240112388fce9dfd
kode <duedumaxwell43@gmail.com>
2024-04-08 10:10:36 -0700

    task 3:deleted __repr__ method

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -74,7 +74,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return ("\n".join("#" * self.__width for _ in range(self.__height)))
-
-    def __repr__(self):
-        """Return a string representation of the rectangle."""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
