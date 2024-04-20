d0248721e94cf4ba1edb37c43be1774868a3b9f9
kode <duedumaxwell43@gmail.com>
2024-04-19 17:48:18 -0700

    task 4:pycodestyle

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -16,4 +16,4 @@ def inherits_from(obj, a_class):
           True if the obj is an instance of a class that inherited
 (directly or indirectly), False otherwise.
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
