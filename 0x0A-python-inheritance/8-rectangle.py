5cef4f62f0c823fd8149a7ea14a46bc7dd9a6ab9
kode <duedumaxwell43@gmail.com>
2024-04-20 06:53:41 -0700

    task 8:pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
 """This program validates an integer"""
-
-
-class BaseGeometry:
-    """
-    Base class for geometry.
-
-    Public instance method:
-        def area(self): Raises an Exception with the message
-                        'area() is not implemented'.
-    """
-    def area(self):
-        """Raises an Exception with the message area() is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates a value as an integer"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
