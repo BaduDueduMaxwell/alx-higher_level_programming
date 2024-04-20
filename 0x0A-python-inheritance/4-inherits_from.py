438fceb810094c81a6a1f639369e6e09547c7a65
kode <duedumaxwell43@gmail.com>
2024-04-19 17:43:19 -0700

    task 4:pycodestyle

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""This function checks if an obj of instance or obj is instance of a class that inherited(directly or indirectly)"""
+"""Check if obj is an instance of a class that\
+inherited (directly or indirectly) from the specified class.
+"""
 
 
 def inherits_from(obj, a_class):
     """
-      This function checks if an obj of instance or obj is instance of a class that inherited(directly or indirectly)
+      Check if obj is an instance of a class that inherited\
+(directly or indirectly) from the specified class.
       Args:
           obj: The object to be checked.
           a_class: The class to compare against.
 
       Returns:
-          True if the obj is an instance of a class that inherited(directly or indirectly), False otherwise.
+          True if the obj is an instance of a class that inherited
+(directly or indirectly), False otherwise.
     """
     return issubclass(type(obj), a_class) and type(obj) != a_class
