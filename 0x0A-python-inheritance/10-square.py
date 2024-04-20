d0929afc77041c0c52b2259a0a5a151a13f97208
kode <duedumaxwell43@gmail.com>
2024-04-20 07:30:12 -0700

    task 10:Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This program inherits from Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangle class"""
+    def __init__(self, size):
+        """Initializes size of square"""
+        super().__init__(size, size)
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """area of a rectangle"""
+        return self.__size * self.__size
