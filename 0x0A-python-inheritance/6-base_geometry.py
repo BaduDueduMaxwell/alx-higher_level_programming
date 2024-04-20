54ec34f0edf96184e77f1a8e285c0eb0c3fb6f7a
kode <duedumaxwell43@gmail.com>
2024-04-19 18:04:17 -0700

    task 6:Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This containes an empty class called BaseGeometry"""
+
+
+class BaseGeometry:
+    def area(self):
+        """Raises an Exception with the message area() is not implemented."""
+        raise Exception("area() is not implemented")
