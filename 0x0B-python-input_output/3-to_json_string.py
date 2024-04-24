159fc0dc92cac99309a7d0d67a6acff909cf3b4a
kode <duedumaxwell43@gmail.com>
2024-04-24 11:21:49 -0700

    to JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """This module returns the JSON representation of an obj"""
 
 
+import json
 def to_json_string(my_obj):
     """Returns the JSON of an obj"""
     return json.dumps(my_obj)
