940e1436e603910a70d3dd97615bd54f6f563612
kode <duedumaxwell43@gmail.com>
2024-04-25 16:41:49 -0700

    task 3:validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,7 +22,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter for width"""
-        if isinstance(self.value, int):
+        if not isinstance(self.value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -36,7 +36,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for height"""
-        if isinstance(self.value, int):
+        if not isinstance(self.value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -50,9 +50,9 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter for x"""
-        if isinstance(self.value, int):
+        if not isinstance(self.value, int):
             raise TypeError("x must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -64,8 +64,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter for y"""
-        if isinstance(self.value, int):
+        if not isinstance(self.value, int):
             raise TypeError("y must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
