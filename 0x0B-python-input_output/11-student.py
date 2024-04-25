848bd68b68bcc876e041962c799e79bf62058ee7
kode <duedumaxwell43@gmail.com>
2024-04-24 17:58:46 -0700

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module defines a class Student"""
+
+
+class Student:
+    """Initialization of attributes"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dict description with data for JSON serialization"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            student_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    student_dict[attr] = getattr(self, attr)
+            return student_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
