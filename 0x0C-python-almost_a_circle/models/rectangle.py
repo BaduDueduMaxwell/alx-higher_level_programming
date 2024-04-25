1086d86cca3ece63a88ef693ce421b90396121aa
kode <duedumaxwell43@gmail.com>
2024-04-25 16:50:37 -0700

    task 3:validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,12 +7,11 @@ class Rectangle(Base):
     """Rectangle class inherits Base class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize Rectangle instance"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
