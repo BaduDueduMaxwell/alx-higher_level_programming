8ccc2cebf10052df7e5e9fd509b72bb89c5d4962
kode <duedumaxwell43@gmail.com>
2024-04-25 16:07:52 -0700

    ignore files

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle class that inherits from Base class"""
+
+
+class Rectangle(Base):
+    """Rectangle class inherits Base class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize Rectangle instance"""
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+        super().__init('Base')
