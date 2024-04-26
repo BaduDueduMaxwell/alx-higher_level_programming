d1b0d5c3516c9794081200f7d66d1a56f7e53955
kode <duedumaxwell43@gmail.com>
2024-04-25 17:15:10 -0700

    task 6:__str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,8 @@ class Rectangle(Base):
         """Prints rectangle with #"""
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.__x, self.__y,\
+                    self.__width, self.__height)
