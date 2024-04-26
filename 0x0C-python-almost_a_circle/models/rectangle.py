b0395cef6ee484b1682221a952cd9ab97245fa1d
kode <duedumaxwell43@gmail.com>
2024-04-25 17:21:35 -0700

    task 7:display#1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,8 +75,10 @@ class Rectangle(Base):
 
     def display(self):
         """Prints rectangle with #"""
-        for _ in range(self.__height):
-            print("#" * self.__width)
+        for i in range(self.__y):
+            print()
+        for i in range(self.__height):
+            print(' ' * self.__x + "#" * self.__width)
 
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}"\
