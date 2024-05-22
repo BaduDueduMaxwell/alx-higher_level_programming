c009368903430e4827997e7f41b9f965c77a2130
kode <duedumaxwell43@gmail.com>
2024-05-22 08:57:53 -0700

    task 7:cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- This script creates the hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL,
 FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
