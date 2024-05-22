2d84de9d7f93cb3220ca043865a0f015a85866af
kode <duedumaxwell43@gmail.com>
2024-05-22 08:43:03 -0700

    task 6:states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- This script creates the database hbtn_0d_usa and the table states
+CREATE DATABASE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL)
