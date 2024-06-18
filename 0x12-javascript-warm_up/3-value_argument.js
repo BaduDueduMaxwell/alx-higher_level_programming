23930fa6a87d65bcd4fdaf948177ad19beb130ae
walkingkode <duedumaxwell43@gmail.com>
2024-06-18 04:01:48 -0700

    task 3:Value of my argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const args = process.argv.slice(1);
+let count = 0;
+
+while (args[count] !== undefined) {
+  count++;
+}
+
+if (count === 0) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
+}
