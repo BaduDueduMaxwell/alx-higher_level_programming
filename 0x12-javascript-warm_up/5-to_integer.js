9f2beafc25513b7c84fdbc1cc7dbc210585195b8
walkingkode <duedumaxwell43@gmail.com>
2024-06-18 04:41:21 -0700

    task 5:An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
-#
+#!/usr/bin/env node
+const args = process.argv.slice(2);
+const num = parseInt(args);
+
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+    console.log('My number:', num);
+}
