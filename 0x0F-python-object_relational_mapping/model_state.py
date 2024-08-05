7cdf8a5ce5c5ada05ce46b0f6a6de55b7b20175d
walkingkode <duedumaxwell43@gmail.com>
2024-08-05 02:41:06 -0700

    task 6:first state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 """
-This script defines a State class and creates an instance of declarative_base
-to link the State class to a MySQL table
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
-from sqlalchemy import Column, Integer, String, create_engine
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import sys
 
 Base = declarative_base()
 
+
 class State(Base):
-    """
-    State class that inherits from Base and links to the MySQL table status.
+    """State class
+
     Attributes:
-        id (int): Uniquere integer, auto-generated, primary key, can't be null.
-        name (str): String with a maximum of 128 characters, can't be null.
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
 
-    if __name__ == "__main__":
-        engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], \
-                sys.argv[2], sys.argv[3]), pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
