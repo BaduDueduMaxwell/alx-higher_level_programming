dc61d8ca256edb2b2dae9ee0df5cf434ffd2cb6b
walkingkode <duedumaxwell43@gmail.com>
2024-08-21 04:01:24 -0700

    task 1:cURL to the end

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script takess in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -sL "$1"
